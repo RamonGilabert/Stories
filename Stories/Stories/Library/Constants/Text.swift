0612426d1de3c1722f35980bcd77b5d9ddb953c2
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-04-19 20:29:34 +0200

    Adds the first welcome files

--- Stories/Stories/Library/Constants/Text.swift
+++ Stories/Stories/Library/Constants/Text.swift
@@ -1,5 +1,9 @@
 import UIKit
+import Sugar
 
 struct Text {
-  
+
+  struct Welcome {
+    static let title = localizedString("welcome")
+  }
 }
---
+++ Stories/Stories/Welcome/WelcomeController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class WelcomeController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
