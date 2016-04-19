20e543dd374b65eca5f20d95724ef276df932c76
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-04-19 19:46:45 +0200

    Deletes the commments

--- Stories/Stories/ViewController.swift
+++ Stories/Stories/ViewController.swift
@@ -1,25 +1,8 @@
-//
-//  ViewController.swift
-//  Stories
-//
-//  Created by Ramon Gilabert Llop on 4/19/16.
-//  Copyright © 2016 BlackBox. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
-
