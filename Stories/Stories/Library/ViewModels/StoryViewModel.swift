3d67d836babf57d4a9e8f3b9fbce18beac5749e1
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-04-24 11:01:40 +0200

    Adds the localizable strings

--- Stories/Stories/Library/ViewModels/StoryViewModel.swift
+++ Stories/Stories/Library/ViewModels/StoryViewModel.swift
@@ -1,5 +1,34 @@
 import UIKit
 
 struct StoryViewModel {
-  
+
+  var title: String
+  var cells: [StoryViewModel]
+  var letter: String?
+  var text: String?
+  var image: String?
+  var footer: String?
+
+  init(title: String = "", cells: [StoryViewModel] = [], letter: String? = nil,
+       text: String? = nil, image: String? = nil, footer: String? = nil) {
+
+    self.title = title
+    self.cells = cells
+    self.letter = letter
+    self.text = text
+    self.image = image
+    self.footer = footer
+  }
+
+  static let story = StoryViewModel(title: "", cells: [
+    StoryViewModel(letter: "R", text: ""),
+    StoryViewModel(image: "R", footer: ""),
+    StoryViewModel(letter: "R", text: ""),
+    ])
+
+  static let motivation = StoryViewModel(title: "", cells: [
+    StoryViewModel(letter: "R", text: ""),
+    StoryViewModel(image: "R", footer: ""),
+    StoryViewModel(letter: "R", text: ""),
+    ])
 }
