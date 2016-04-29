35224b337724d800d888e2d07e0a552437f6de37
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-04-30 00:25:14 +0200

    Adds all the constants to the engine

--- Stories/Stories/Engine/Engine/Engine.swift
+++ Stories/Stories/Engine/Engine/Engine.swift
@@ -3,15 +3,18 @@ import UIKit
 struct Engine {
 
   static let initialTexts = [
-    Text.Interactive.one,
-    Text.Interactive.two,
-    Text.Interactive.three,
-    Text.Interactive.four,
-    Text.Interactive.five,
-    Text.Interactive.six,
-    Text.Interactive.seven,
-    Text.Interactive.eight,
-    Text.Interactive.nine
+    Text.Interactive.one, Text.Interactive.two, Text.Interactive.three,
+    Text.Interactive.four, Text.Interactive.five, Text.Interactive.six,
+    Text.Interactive.seven, Text.Interactive.eight, Text.Interactive.nine,
+    Text.Interactive.ten, Text.Interactive.eleven, Text.Interactive.twelve,
+    Text.Interactive.thirteen, Text.Interactive.fourteen, Text.Interactive.fiveteen,
+    Text.Interactive.sixteen, Text.Interactive.seventeen, Text.Interactive.eighteen,
+    Text.Interactive.nineteen, Text.Interactive.twenty, Text.Interactive.twentyOne,
+    Text.Interactive.twentyTwo, Text.Interactive.twentyThree, Text.Interactive.twentyFour,
+    Text.Interactive.twentyFive, Text.Interactive.twentySix, Text.Interactive.twentySeven,
+    Text.Interactive.twentyEight, Text.Interactive.twentyNine, Text.Interactive.thirty,
+    Text.Interactive.thirtyOne, Text.Interactive.thirtyTwo, Text.Interactive.thirtyThree,
+    Text.Interactive.thirtyFour, Text.Interactive.thirtyFive, Text.Interactive.thirtySix
   ]
 
   static let initialButtons = [
@@ -20,7 +23,27 @@ struct Engine {
     (first: Text.Interactive.Buttons.fourOne, second: Text.Interactive.Buttons.fourTwo,
       key: "four", nextFirst: 9, nextSecond: 6),
     (first: Text.Interactive.Buttons.sevenOne, second: Text.Interactive.Buttons.sevenTwo,
-      key: "seven", nextFirst: 9, nextSecond: 8)
+      key: "seven", nextFirst: 9, nextSecond: 8),
+    (first: Text.Interactive.Buttons.nineOne, second: Text.Interactive.Buttons.nineTwo,
+      key: "nine", nextFirst: 10, nextSecond: 11),
+    (first: Text.Interactive.Buttons.elevenOne, second: Text.Interactive.Buttons.elevenTwo,
+      key: "eleven", nextFirst: 12, nextSecond: 13),
+    (first: Text.Interactive.Buttons.thirteenOne, second: Text.Interactive.Buttons.thireteenTwo,
+      key: "thirteen", nextFirst: 14, nextSecond: 14),
+    (first: Text.Interactive.Buttons.fourteenOne, second: Text.Interactive.Buttons.fourteenTwo,
+      key: "fourteen", nextFirst: 15, nextSecond: 18),
+    (first: Text.Interactive.Buttons.seventeenOne, second: Text.Interactive.Buttons.seventeenTwo,
+      key: "seventeen", nextFirst: 19, nextSecond: 19),
+    (first: Text.Interactive.Buttons.twentyTwoOne, second: Text.Interactive.Buttons.twentyTwoTwo,
+      key: "twenty-two", nextFirst: 23, nextSecond: 23),
+    (first: Text.Interactive.Buttons.twentyFiveOne, second: Text.Interactive.Buttons.twentyFiveTwo,
+      key: "twenty-five", nextFirst: 26, nextSecond: 26),
+    (first: Text.Interactive.Buttons.twentyEightOne, second: Text.Interactive.Buttons.twentyEightTwo,
+      key: "twenty-eight", nextFirst: 29, nextSecond: 30),
+    (first: Text.Interactive.Buttons.thirtyOneOne, second: Text.Interactive.Buttons.thirtyOneTwo,
+      key: "thirty-one", nextFirst: 34, nextSecond: 32),
+    (first: Text.Interactive.Buttons.thirtySixOne , second: Text.Interactive.Buttons.thirtySixTwo,
+      key: "thirty-six", nextFirst: 36, nextSecond: 36)
   ]
 
   static func response(button: String) -> [String] {
